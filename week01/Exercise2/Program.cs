0b434a12a8f70f55ef9c485dd37a279dcc27c2ac
Krelmoon <Krelmoon@gmail.com>
2026-01-10 13:25:59 -0500

    first draft

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("What is your grade percentage? ");
+        string userInput = Console.ReadLine();
+        int grade = int.Parse(userInput);
+        string letter = ("F");
+        string sign = (" ");
+        if (grade >= 90)
+        {
+            letter = ("A");
+        }
+        else if (grade >= 80)
+        {
+            letter = ("B");
+        }
+        else if (grade >= 70)
+        {
+            letter = ("C");
+        }
+        else if (grade >= 60)
+        {
+            letter = ("D");
+        }
+        else if (grade < 60)
+        {
+            letter = ("F");
+        }
+        int remainder = (grade % 10);
+        if (remainder >= 7 && grade < 97 && grade >= 60)
+        {
+            sign = ("+");
+        }
+        else if (remainder <3 && grade >= 60)
+        {
+            sign = ("-");
+        }
+        else
+        {
+            sign = (" ");
+        }
+        Console.WriteLine($"Your grade is {letter}{sign} ");
     }
 }
--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 100);
+        int guess;
+        do
+        {
+           Console.WriteLine("What is your guess? ");
+            string userInput2 = Console.ReadLine();
+            guess = int.Parse(userInput2);
+            if (number > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (number < guess)
+            {   
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            } 
+        } while (number != guess);
+        
     }
 }
