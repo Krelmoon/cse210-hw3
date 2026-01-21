f1b55531077f9cdb3ee98142aa56fd7477fbfea4
Krelmoon <Krelmoon@gmail.com>
2026-01-21 09:33:38 -0500

    Fraction

---
+++ week03/Fractions/Fraction.cs
@@ -0,0 +1,37 @@
+// works with Fractions
+public class Fraction
+{
+    // attributes
+    private int _top;
+    private int _bottom;
+
+    // constructors
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+
+    public Fraction(int wholeNumber)
+    {
+        _top = wholeNumber;
+        _bottom = 1;
+    }
+
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+
+    // methods
+    public string GetFractionString()
+    {
+        return $"{_top}/{_bottom}";
+    }
+
+    public double GetDecimalValue()
+    {
+        return (double)_top / (double)_bottom;
+    }
+}
